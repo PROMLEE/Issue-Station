f9a04d8f0a6c118c572525c455ad7bfef5c643b3
Easthee <132530028+easthee@users.noreply.github.com>
2024-05-23 20:24:30 +0900

    Delete src directory
    ,

--- src/Developer.java
+++
@@ -1,9 +0,0 @@
-public class Developer extends Account {
-    public void changeIssueStatus(Issue issue) {
-        // Implementation here
-    }
-
-    public void addComment(String comment) {
-        // Implementation here
-    }
-}
