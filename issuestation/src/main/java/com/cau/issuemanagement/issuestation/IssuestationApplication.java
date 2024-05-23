1601742e98867d962a81aa33b63051fa08bb8ed4
dongheeHan <ark182818@gmail.com>
2024-05-23 20:35:28 +0900

    first

---
+++ issuestation/src/main/java/com/cau/issuemanagement/issuestation/IssuestationApplication.java
@@ -0,0 +1,13 @@
+package com.cau.issuemanagement.issuestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuestationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuestationApplication.class, args);
+	}
+
+}
