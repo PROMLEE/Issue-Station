9b67d9de760189b13f6bbb8c13320953634ce554
PROMLEE <donghoon099@naver.com>
2024-05-28 21:49:53 +0900

    token 수정

--- issuestation/src/main/java/com/issuestation/Controller/ProjectController.java
+++ issuestation/src/main/java/com/issuestation/Controller/ProjectController.java
@@ -44,6 +44,7 @@ import com.issuestation.apiPayload.ApiResponse;
 import com.issuestation.apiPayload.code.status.ErrorStatus;
 import com.issuestation.apiPayload.exception.handler.TempHandler;
 import com.issuestation.converter.ProjectCreateConverter;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,56 +56,21 @@ import org.springframework.web.bind.annotation.*;
 public class ProjectController {
 
     private final ProjectCreateService projectService;
-    @Autowired  private TokenProvider tokenProvider;
+    @Autowired  TokenProvider tokenProvider;
 
     @PostMapping("/create")
-    public ApiResponse<ProjectResponseDto.JoinProjectCreateResponseDto> join(@RequestHeader("Authorization") String token, @RequestBody @Valid ProjectRequestDto.JoinProjectCreateRequestDto request) {
-        String jwtToken = token.replace("Bearer ", "");
+    public ApiResponse<ProjectResponseDto.JoinProjectCreateResponseDto> join(HttpServletRequest token, @RequestBody @Valid ProjectRequestDto.JoinProjectCreateRequestDto request) {
 
         //토큰 검증
+        var getToken = token.getHeader("Authorization");
+        String jwtToken = getToken.replace("Bearer ", "");
         String loginId;
         try {
             loginId = tokenProvider.validateJwt(jwtToken);
         } catch (Exception e) {
-            throw new TempHandler(ErrorStatus.TEMP_EXCEPTION);
+            throw new TempHandler(ErrorStatus._UNAUTHORIZED);
         }
         Project project = projectService.joinProject(request);
         return ApiResponse.onSuccess(ProjectCreateConverter.toProjectDto(project));
     }
-
 }
-//package com.cau.issuemanagement.issuestation.Controller;
-//
-//import com.cau.issuemanagement.issuestation.Dto.ProjectCreateDto;
-//import com.cau.issuemanagement.issuestation.Dto.ResponseDto;
-//import com.cau.issuemanagement.issuestation.Security.TokenProvider;
-//import com.cau.issuemanagement.issuestation.Service.ProjectService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/project")
-//public class ProjectController {
-//
-//    @Autowired private ProjectService projectService; //서비스는 db랑 상호작용하기위해 필요함
-//    @Autowired private TokenProvider tokenProvider; //토큰 가져와야함 여기서
-//
-//    @PostMapping("/create")
-//    public ResponseDto<?> createProject(@RequestHeader("Authorization") String token, @RequestBody ProjectCreateDto projectCreateDto) {
-//        //Bearer 토큰에서 값 추출
-//        String jwtToken = token.replace("Bearer ", "");
-//
-//        //토큰 검증
-//        String userId;
-//        try {
-//            userId = tokenProvider.validateJwt(jwtToken);
-//        } catch (Exception e) {
-//            return ResponseDto.setFailed("Invalid token", null);
-//        }
-//
-//        //토큰이 유효할 경우에만 프젝생성 가능하도록
-//        ResponseDto<?> result = projectService.createProject(projectCreateDto, userId);
-//        System.out.println(result.toString());
-//        return result;
-//    }
-//}
