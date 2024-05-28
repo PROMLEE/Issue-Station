16fabe6c59c285e9b37db7338155dcc3143d76af
jeoneunjin <pengom0809@gmail.com>
2024-05-28 16:39:54 +0900

    proj detail screen 수정 중

--- main.py
+++ main.py
@@ -30,7 +30,9 @@ class MainWindow(QMainWindow):
         self.ui.user_name.setText('Unknown')
         self.ui.userBtn.clicked.connect(self.setUserWidget)
         self.ui.user_widget.hide()
-    
+        ## 검색 키워드
+        self.search_keyword = None
+        ## 로그인 화면 
         self.login = False
         ## track of uwer_widget hidden or not
         self.user_widget_hidden = True
@@ -42,27 +44,33 @@ class MainWindow(QMainWindow):
         else:
             self.ui.user_widget.hide()
             self.user_widget_hidden = True
-    ## Function for setting project list screen
-    def go_to_project_screen(self):
-        self.ui.stackedWidget.setCurrentIndex(2)
-        self.setDataTable()
+
     ## Function for searching
     def on_searchBtn_clicked(self):
         search_text = self.ui.searchInput.text().strip()
         if search_text :
-            ##self.ui.searchResult.setText(search_text)
+            ## keyword 저장해둠
+            self.search_keyword = search_text
+            ## 스크린 이동
             self.go_to_project_screen()
         if not search_text :
             self.ui.label_31.setText("검색어를 입력해주세요.")
-    
-    
-    ## proj detail 화면으로
-    def go_to_projdetail(self,id,name):
-        self.detail = ProjDetailScreen(id,name,self)
+##===================Project Screen===============================##
+    ## project screen 
+    def go_to_project_screen(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+        self.setDataTable()
+            
+    ## proj detail screen 
+    def go_to_projdetail(self,dict):
+        self.detail = ProjDetailScreen(dict,self)
         self.detail.exec()
 
     ## Fuction for setting project data
     def setDataTable(self):
+        ## search keyword가 있을 경우
+        if self.search_keyword:
+            keyword = self.search_keyword
         table_widget = self.ui.project_table
         json_data = getJsonData('./assets/projectlist_dump.json')
         proj_list = json_data['projectlist']
@@ -73,7 +81,7 @@ class MainWindow(QMainWindow):
             button_name = 'issue_btn_' + str(row)
             self.button = QPushButton("Open", self)
             self.button.setAccessibleName(button_name)
-            self.button.clicked.connect(lambda _, id=proj_list[row]['projectid'], name=proj_list[row]['name']: self.go_to_projdetail(id,name))
+            self.button.clicked.connect(lambda _, dict = proj_list[row] : self.go_to_projdetail(dict))
  
             ##setting table data
             table_widget.setItem(row,0,QTableWidgetItem(str(proj_list[row]['projectid'])))
@@ -86,7 +94,10 @@ class MainWindow(QMainWindow):
             table_widget.setCellWidget(row,4,self.button) 
             table_widget.setItem(row,5,QTableWidgetItem(proj_list[row]['initdate']))
             table_widget.setItem(row,6,QTableWidgetItem(proj_list[row]['description']))  
-    
+        
+##===========================================================================================
+
+##===========================================================================================
     ## Change QPushButton Checkable status when stackedWidget index changed
     def on_stackedWidget_currentChanged(self, index):
         btn_list = self.ui.icon_only_widget.findChildren(QPushButton) \
@@ -149,6 +160,7 @@ class MainWindow(QMainWindow):
         self.login = LoginScreen()      
         self.login.exec()
         self.show()
+##====================================================================================
     
 ##로그인 화면
 class LoginScreen(QDialog):
@@ -170,6 +182,7 @@ class LoginScreen(QDialog):
         self.signup.exec()
         self.show()
 
+##================================================================================
 ##계정만들기 화면
 class SignupScreen(QDialog):
     def __init__(self):
@@ -183,16 +196,18 @@ class SignupScreen(QDialog):
     def back_to_login(self):
         self.close()
     ##계정 만들기 O -> 로그인 화면으로
-
+##==============================================================================
 ##project detail 화면
 class ProjDetailScreen(QDialog):
-    def __init__(self, id, name, parent=None):
+    def __init__(self, dict, parent=None):
         super(ProjDetailScreen, self).__init__(parent)
         self.ui = uic.loadUi("proj_detail.ui", self)
-        self.setWindowTitle(name)
+        self.setWindowTitle(dict['name'])
         self.setWindowIcon(QtGui.QIcon('Logo_11.png'))
         self.show()
-        self.ui.temp.setText(str(id))
+        self.ui.project_id.setText(str(dict['projectid']))
+        self.ui.project_name.setText(dict['name'])
+        self.ui.description.setText(dict['description'])
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
