64d70266e685d8f46e19bd3fb549f214e4d38c84
PROMLEE <donghoon099@naver.com>
2024-06-03 12:12:27 +0900

    Fixer 정보 열람 추가

--- issuestation/src/main/java/com/issuestation/Repository/FixerRepository.java
+++ issuestation/src/main/java/com/issuestation/Repository/FixerRepository.java
@@ -3,5 +3,8 @@ package com.issuestation.Repository;
 import com.issuestation.Entity.Fixer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FixerRepository extends JpaRepository<Fixer, Long> {
+    Optional<Fixer> findDistinctFirstByIssueId(Long issueId);
 }
